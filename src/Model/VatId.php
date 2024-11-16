33d889e72b75f0d11129cd9db40c0246b71f06cc
M van Dam <dragonbe+github@gmail.com>
2024-11-16 16:37:22 +0100

    Defining the interface for VatID
    Ensuring we can retrieve the components of a complete VAT ID so we can process them correctly

--- src/Model/VatId.php
+++ src/Model/VatId.php
@@ -5,4 +5,17 @@ namespace Dragonbe\ClarityCompass\Model;
 
 final class VatId implements VatIdInterface
 {
+	/**
+     * Retrieve the country prefix from the VAT ID
+     *
+     * @return string
+     */ 
+	public function getCountryPrefix(): string;
+    
+    /**
+     * Retrieve the VAT identifier
+     *
+     * @return string
+     */
+    public function getVatId(): string;
 }
