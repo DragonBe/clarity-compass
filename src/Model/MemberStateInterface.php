ebf84706e74f2ea709f475f840128b8193d664cd
M van Dam <dragonbe+github@gmail.com>
2024-11-23 00:02:41 +0100

    Defining a minimal interface for a member state
    Providing an interface to retrieve the country code and the English name of a member state

---
+++ src/Model/MemberStateInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+declare('strict_types=1');
+
+namespace Dragonbe\ClarityCompass\Model;
+
+interface MemberStateInterface
+{
+    /**
+     * Retrieve the two-letter country code of a member
+     * state.
+     *
+     * @return string
+     */
+    public function getCountryCode(): string;
+    
+    /**
+     * Retrieve the English name of a member state.
+     *
+     * @return string
+     */ 
+    public function getMemberStateName(): string;
+}
