198f6f795b9090c84f632e4a2bb29dcab40cc281
M van Dam <dragonbe+github@gmail.com>
2024-11-22 23:47:33 +0100

    A member state must implement interface
    Each member state must implement a member state interface

---
+++ tests/unit/Model/MemberStateTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare('strict_types=1');
+
+namespace DragonBe\ClarityCompassTest\Model;
+
+use DragonBe\ClarityCompass\Model\MemberState;
+use DragonBe\ClarityCompass\Model\MemberStateInterface;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(MemberState::class)]
+class MemberStateTest extends TestCase
+{
+    #[Test]
+    public function testMemberStateImplementsMemberStateInterface(): void
+    {
+        $memberState = new MemberState();
+        $this->assertInstanceOf(MemberStateInterface::class);
+    }
+}
