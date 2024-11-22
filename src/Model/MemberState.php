9c96c39a7041ae9217db04b726c5c661df584cf8
M van Dam <dragonbe+github@gmail.com>
2024-11-23 00:11:12 +0100

    Creating a member state value object
    A member state value object will become useful for working with member states

---
+++ src/Model/MemberState.php
@@ -0,0 +1,35 @@
+<?php
+
+declare('strict_types=1');
+
+namespace DragonBe\ClarityCompass\Model;
+
+final class MemberState implements MemberStateInterface
+{
+    private string $countryCode;
+    private string $memberStateName;
+    
+    public function __construct(
+        string $countryCode = '',
+        string $memberStateName = ''
+    ) {
+        $this->countryCode = $countryCode;
+        $this->memberStateName = $memberStateName;
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function getCountryCode(): string
+    {
+        return $this->countryCode();
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function getMemberStateName(): string
+    {
+        return $this->memberStateName();
+    }
+}
